96bab791ab34b830884d8672c8490ecfa7332882
rakanya-dev <earlwinmark@gmail.com>
2026-03-28 16:28:16 +0800

    fix16

--- api/index.php
+++ api/index.php
@@ -1,2 +1,21 @@
 <?php
+
+// 1. Create temporary writable folders for Laravel in Vercel's /tmp directory
+$tmpDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs'
+];
+
+foreach ($tmpDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+// 2. Tell Laravel to use these temporary folders
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+
+// 3. Boot Laravel
 require __DIR__ . '/../public/index.php';
